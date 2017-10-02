e880159925a96c59d49c73545e6647097b6819c2
Dajan_Stjepanovic <dajan.stjepanovic@hotmail.com>
2017-10-02 20:38:01 +0200

    Eo skoro sve treba doraditi dizajn register nmg vise

--- resources/views/auth/registers.blade.php
+++ resources/views/auth/registers.blade.php
@@ -1,9 +1,9 @@
-@extends('layouts.app')
+@include('layouts.header')
 
-@section('content')
+@include('layouts.nav')
 <div class="container">
     <div class="row">
-        <div class="col-md-8 col-md-offset-2">
+        <div class="col-md-8 ">
             <div class="panel panel-default">
                 <div class="panel-heading">Register</div>
 
@@ -12,7 +12,7 @@
                         {{ csrf_field() }}
 
                         <div class="form-group{{ $errors->has('name') ? ' has-error' : '' }}">
-                            <label for="name" class="col-md-4 control-label">Name</label>
+                            <label for="name" class="col-md-10 control-label">Name</label>
 
                             <div class="col-md-6">
                                 <input id="name" type="text" class="form-control" name="name" value="{{ old('name') }}" required autofocus>
@@ -103,4 +103,4 @@
         </div>
     </div>
 </div>
-@endsection
+@include('layouts.footer')
