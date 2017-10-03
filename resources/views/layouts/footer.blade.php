80a644db0a691dcf6fc7e0df7d07daa56dcc155c
KojaKojaKoja <koja1402@gmail.com>
2017-10-03 15:37:42 +0200

    Ubacen footer

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,3 +1,14 @@
+
+<section class="footer">
+<div class="container">
+  <div class="row">
+   <div class="col-md-12">
+      <p class="copyright"> Copyright © Mantos 2017. All right reserved. </p>
+    </div>
+    </div>
+    </div>
+</section>
+
   <script type="text/javascript" src="plugins/jquery/jquery.min.js"></script>
         <script type="text/javascript" src="plugins/bootstrap/js/tether.min.js"></script>
         <script type="text/javascript" src="plugins/bootstrap/js/bootstrap.min.js"></script>
