c308d4b4f9a10280beee02c0e3b4cb2a5263dbd7
Aleksandar <aleksandar.maslic@yahoo.com>
2017-10-07 19:39:29 +0200

    color picker implemented

--- resources/views/addteritory.blade.php
+++ resources/views/addteritory.blade.php
@@ -6,9 +6,10 @@
 
 <div class="container-main">
 
-    <div id="map-canvas" style="width: 620px;
+   <!-- <div id="map-canvas" style="width: 620px;
+    height: 480px; padding-top: 150px;" ></div>-->
+    <div id="map" style="width: 620px;
     height: 480px; padding-top: 150px;" ></div>
-
      <select>
         @foreach($users as $user)
             <option name="{{$user->id}}">
@@ -16,17 +17,57 @@
             </option>
         @endforeach
     </select>
+    <div>
+        Color: <input class="jscolor color-picker" value="ab2567">
+    </div>
 </div>
 
 
-<script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=AIzaSyC2_-ZK1vYH7btuM7Qoz5anEajPXI5YtiM">
-</script>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+
 
 <script type="text/javascript">
 
     $(document).ready(function(){
-        var poly;
+
+      
+
+        initMap = function () {
+        var map = new google.maps.Map(document.getElementById('map'), {
+          center: {lat: -34.397, lng: 150.644},
+          zoom: 8
+        });
+
+        function rgb2hex(rgb) {
+            rgb = rgb.match(/^rgb\((\d+),\s*(\d+),\s*(\d+)\)$/);
+            function hex(x) {
+                return ("0" + parseInt(x).toString(16)).slice(-2);
+            }
+            return "#" + hex(rgb[1]) + hex(rgb[2]) + hex(rgb[3]);
+        }
+
+        var drawingManager = new google.maps.drawing.DrawingManager({
+          drawingMode: google.maps.drawing.OverlayType.MARKER,
+          drawingControl: true,
+          drawingControlOptions: {
+            position: google.maps.ControlPosition.TOP_CENTER,
+            drawingModes: ['polygon']
+          },
+          markerOptions: {icon: 'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png'},
+          circleOptions: {
+            fillColor: rgb2hex($(".color-picker").css("backgroundColor")),
+            fillOpacity: 1,
+            strokeWeight: 5,
+            clickable: false,
+            editable: true,
+            zIndex: 1
+          }
+        });
+        drawingManager.setMap(map);
+        alert( rgb2hex($(".color-picker").css("backgroundColor")));
+      }
+
+/*       var poly;
         var map;
         var image = {
             url: "https://maps.gstatic.com/mapfiles/ms2/micons/red.png",
@@ -48,7 +89,8 @@
         var polyOptions = {
             strokeColor: '#000000',
             strokeOpacity: 1.0,
-            strokeWeight: 2
+            strokeWeight: 2,
+            fillColor: "#000000"
         };
         poly = new google.maps.Polyline(polyOptions);
         google.maps.event.addListener(map, 'click', addLatLng);
@@ -75,9 +117,11 @@
 
             console.log("Lat: " + markerLat + " Lng: " + markerLng)
                
-        }
+        } */
     });
 </script>
+<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyC2_-ZK1vYH7btuM7Qoz5anEajPXI5YtiM&libraries=drawing&callback=initMap"
+         async defer></script>
 
 
          
--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -12,6 +12,7 @@
   <script type="text/javascript" src="plugins/bootstrap/js/tether.min.js"></script>
   <script type="text/javascript" src="plugins/bootstrap/js/bootstrap.min.js"></script>
   <script src="{{ asset('js/custom_style.js') }}"></script>
+   <script src="{{ asset('js/jscolor.min.js') }}"></script>
   
 </body>
 </html>
