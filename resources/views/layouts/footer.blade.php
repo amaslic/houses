2140df6176a9d5e3057ebce4dbc500f481f517a0
Dajan_Stjepanovic <dajan.stjepanovic@hotmail.com>
2017-10-16 22:58:59 +0200

    izmjene

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,24 +1,47 @@
   <footer class="footer">
+
     <div class="container">
+
       <div class="row">
+
         <div class="col-md-12">
+
           <p class="copyright"> Copyright © Mantos 2017. All right reserved. </p>
+
         </div>
+
       </div>
+
     </div>
+
   </footer>
 
+
+
   <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
+
   <script type="text/javascript" src="https://cdn.rawgit.com/prashantchaudhary/ddslick/master/jquery.ddslick.min.js" ></script>
-  <script type="text/javascript" src="/plugins/jquery/jquery.min.js"></script>
-  <script type="text/javascript" src="/plugins/bootstrap/js/tether.min.js"></script>
-  <script type="text/javascript" src="/plugins/bootstrap/js/bootstrap.min.js"></script>
-  <script src="{{ asset('js/custom_style.js') }}"></script>
-  <script src="{{ asset('js/jscolor.min.js') }}"></script>
-  <script src="{{ asset('js/ddslick.js') }}"></script>
-  <script src="/js/msdropdown/jquery.dd.js" type="text/javascript"></script>
-  <script src="/js/msdropdown/jquery.dd.min.js" type="text/javascript"></script>
+
+
+
+ 
+<script src="{{ asset('plugins/jquery/jquery.min.js') }}"  type="text/javascript" ></script>
+    <script src="{{ asset('plugins/bootstrap/js/tether.min.js') }}"  type="text/javascript"></script>
+  <script src="{{ asset('plugins/bootstrap/js/bootstrap.min.js') }}"  type="text/javascript"></script>
+  <script src="{{ asset('js/custom_style.js') }}"  type="text/javascript"></script>
+
+  <script src="{{ asset('js/jscolor.min.js') }}"  type="text/javascript" ></script>
+
+  <script src="{{ asset('js/ddslick.js') }}"  type="text/javascript"></script>
+
+  <script src="{{ asset('js/msdropdown/jquery.dd.js') }}"  type="text/javascript"></script>
+
+  <script src="{{ asset('js/msdropdown/jquery.dd.min.js') }}"  type="text/javascript"></script>
+
   
+
   
+
 </body>
+
 </html>
--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,44 +1,86 @@
-
 <!DOCTYPE html>
+
 <html lang="en">
+
     <head>
+
         <meta charset="utf-8">
+
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
+
         <meta name="viewport" content="width=device-width, initial-scale=1">
+
         <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+
         <title>Mantos Admin</title>
 
+
+
         <!-- WEB FONTS : use %7C instead of | (pipe) -->
+
         <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400%7CRaleway:300,400,500,600,700%7CLato:300,400,400italic,600,700" rel="stylesheet" type="text/css" />
 
+
+
         <!-- CORE CSS -->
-        <link href="/plugins/bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
-        <link href="/plugins/metis-menu/metisMenu.min.css" rel="stylesheet" type="text/css" />
-        <link href="/plugins/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css" />
-        <link href="/plugins/simple-line-icons-master/css/simple-line-icons.css" rel="stylesheet" type="text/css" />
-        <link href="/plugins/animate/animate.css" rel="stylesheet" type="text/css" />
-        <link href="/plugins/c3/c3.min.css" rel="stylesheet">
-        <link href="/plugins/widget/widget.css" rel="stylesheet">
-        <link href="/plugins/calendar/fullcalendar.min.css" rel="stylesheet">
-        <link href="/plugins/ui/jquery-ui.css" rel="stylesheet">
 
-         <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
-    <script type="text/javascript" src="https://cdn.rawgit.com/prashantchaudhary/ddslick/master/jquery.ddslick.min.js" ></script>
+         <link href="{{ asset('plugins/bootstrap/css/bootstrap.min.css')}}" rel="stylesheet" type="text/css" />
 
-        <!-- THEME CSS -->
-        <link href="/css/style.css" rel="stylesheet" type="text/css" />
-         <link href="/css/dajanstyle.css" rel="stylesheet" type="text/css" />
-        <link href="/css/zoranstyle.css" rel="stylesheet" type="text/css" />
-        <link href="/css/theme/dark.css" rel="stylesheet" type="text/css" />
-        <link rel="stylesheet" type="text/css" href="/css/msdropdown/dd.css" />
+        <link href="{{ asset('plugins/metis-menu/metisMenu.min.css')}}" rel="stylesheet" type="text/css" />
+        <link href="{{ asset('plugins/font-awesome/css/font-awesome.min.css')}}" rel="stylesheet" type="text/css" />
+        <link href="{{ asset('plugins/simple-line-icons-master/css/simple-line-icons.css')}}" rel="stylesheet" type="text/css" />
+        <link href="{{ asset('plugins/animate/animate.css')}}" rel="stylesheet" type="text/css" />
+        <link href="{{ asset('plugins/c3/c3.min.css')}}" rel="stylesheet" type="text/css" />
+        <link href="{{ asset('plugins/widget/widget.css')}}" rel="stylesheet" type="text/css" />
+        <link href="{{ asset('plugins/calendar/fullcalendar.min.css')}}" rel="stylesheet" type="text/css" />
+         <link href="{{ asset('plugins/ui/jquery-ui.css')}}" rel="stylesheet" type="text/css" />
+
+       
+       
+
+       
 
         
+
+       
+
+
+         <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
+
+    <script type="text/javascript" src="https://cdn.rawgit.com/prashantchaudhary/ddslick/master/jquery.ddslick.min.js" ></script>
+
+
+
+        <!-- THEME CSS -->
+        <link href="{{ asset('css/style.css')}}" rel="stylesheet" type="text/css" />
+        <link href="{{ asset('css/dajanstyle.css')}}" rel="stylesheet" type="text/css" />
+         <link href="{{ asset('css/zoranstyle.css')}}" rel="stylesheet" type="text/css" />
+         <link href="{{ asset('css/theme/dark.css')}}" rel="stylesheet" type="text/css" />
+         <link href="{{ asset('css/msdropdown/dd.css')}}" rel="stylesheet" type="text/css" />
+        
+
+         
+
+    
+
+
+
+        
+
         <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+
         <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+
         <!--[if lt IE 9]>
+
           <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+
           <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+
         <![endif]-->
+
     </head>
+
     <body>
+
     
